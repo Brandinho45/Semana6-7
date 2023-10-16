6c92c1e28c8906ad564bba392d67799ff54907cd
Bradon Isla <islacconislla@gmail.com>
2023-10-16 10:56:39 -0500

    Semana6-7

---
+++ grabar_archivo.py
@@ -0,0 +1,5 @@
+
+archivo = open ("archivo_de_prueba.txt","wt")
+contenido = "Línea1 hola amigos como están?\nLínea2 Bienvenido a Untels. Escribir el contenido"
+archivo.write(contenido)
+archivo.close()
